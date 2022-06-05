dca33a41db9dd64efb989aa72e957a984649a73d
Dewa Ade-onojobi <adewalade@gmail.com>
2022-06-05 22:35:00 +0100

    ft-add Javastript for Attack Btn

---
+++ app.js
@@ -0,0 +1,25 @@
+const getRandomVal = ( min, max ) => {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+const app = Vue.createApp({
+    data () {
+        return {
+            playerHP: 200,
+            monsterHP: 200
+        }
+    },
+    methods: {
+        attackMonster () {
+            const attackPoints = getRandomVal(5, 12);
+            this.monsterHP -= attackPoints;
+            this.attackPlayer();
+        },
+        attackPlayer () {
+            const attackPoints = getRandomVal(8, 15);
+            this.playerHP -= attackPoints;
+        }
+    }
+})
+
+app.mount("#game")
